cefb3f4866bd9b77ad8dc246c9b826a46c7bd158
Akash-2021115314 <akashpanneer2004@gmail.com>
2026-03-24 18:23:11 +0530

    method1

---
+++ src/main/java/com/netz/eurekaApplication/newones.java
@@ -0,0 +1,9 @@
+package com.netz.eurekaApplication;
+
+public class newones {
+
+	   public void method2() {
+		    System.out.println("Multiplication: ");
+		    System.out.println("Division: ");
+			}
+}
