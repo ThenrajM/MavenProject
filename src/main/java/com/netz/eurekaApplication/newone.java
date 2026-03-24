d1b4e91032aefce0c97f9771e57aa12eb25ee59f
Akash-2021115314 <akashpanneer2004@gmail.com>
2026-03-24 12:31:51 +0530

    new sys

--- src/main/java/com/netz/eurekaApplication/newone.java
+++ src/main/java/com/netz/eurekaApplication/newone.java
@@ -4,6 +4,11 @@ public class newone {
 	public static void main(String[] args) {
         
         System.out.println("NEW: ");
+        
+        System.out.println("ADD: ");
+        
+        System.out.println("SUB: ");
 
     }
+	
 }
